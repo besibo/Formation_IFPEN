ffa3b8a8ccb468ae27e037b222ec0476a6aa9f80
Benoit Simon-Bouhet <besibo@gmail.com>
2025-06-26 15:17:29 +0200

    Ajout données

--- packages.R
+++ packages.R
@@ -1,22 +1,23 @@
-install.packages("tidyverse")
-install.packages("patchwork")
-install.packages("ggtext")
-install.packages("skimr")
-install.packages("car")
-install.packages("readxl")
-install.packages("janitor")
-install.packages("colorspace")
+install.packages("tidyverse")   # Pour les graphiques et la manipulation de données
+install.packages("patchwork")   # Pour associer plusieurs graphiques en un
+install.packages("ggtext")      # Pour formatter les titres des graphiques
+install.packages("skimr")       # Pour produire des resumés statistiques
+install.packages("car")         # Pour le test de Levene (comp variances)
+install.packages("readxl")      # Pour importer des fichiers Excel
+install.packages("janitor")     # Pour nettoyer facilement les noms de variables
+install.packages("colorspace")  # Pour la gestion (avancée) des palettes de couleur
 
 # Newly added!
-install.packages("scales")
-install.packages("pwr")
-install.packages("effectsize")
-install.packages("rstatix")
-install.packages("Hmisc")
-install.packages("rpart")
-install.packages("rpart.plot")
-install.packages("ade4")
+install.packages("scales")     # Pour la mise en forme des axes des graphiques
+install.packages("pwr")        # Pour les tests de puissance
+install.packages("effectsize") # Pour calculer le d de Cohen
+install.packages("rstatix")    # Pour les tests stats "tidy"
+install.packages("Hmisc")      # Pour les IC95%
+install.packages("rpart")      # Pour les arbres de régression/classification
+install.packages("rpart.plot") # Pour visualiser les arbres
+install.packages("ade4")       # Pour les ACP/AFC
 
-install.packages("nycflights13")
-install.packages("palmerpenguins")
+# Jeux de données
+install.packages("nycflights13")   # Les vols au départ de New York en 2013
+install.packages("palmerpenguins") # Données sur les manchots de terre Adélie
 
